edbb0144e0b5522f1dbc99d5409a60acaad1c8fe
Sekai02 <luisalejandroarteagamorales@gmail.com>
2024-07-31 11:36:39 -0400

    raw mode added to START_MESSAGE scene to prevent user from writing during dialogues

--- start.c
+++ start.c
@@ -1,27 +1,38 @@
 #include "start.h"
 #include "typewriter.h"
+#include "iohandler.h"
 
 #include <unistd.h>
 #include <stdio.h>
 
 void drawStartMessage(){
+    enableRawMode();
     typeTextWithSound("Welcome soldier!\n");
     sleep(1);
+    disableRawMode();
     printf("\n");
 
+    enableRawMode();
     typeTextWithSound("Defeat the PROMPT INVADERS and come back a hero.\n");
     sleep(1);
+    disableRawMode();
     printf("\n");
 
+    enableRawMode();
     typeTextWithSound("Your operating system is depending upon you.\n");
     sleep(1);
+    disableRawMode();
     printf("\n");
 
+    enableRawMode();
     typeTextWithSound("Good luck.\n");
     sleep(1);
+    disableRawMode();
     printf("\n");
 
+    enableRawMode();
     typeTextWithSound("Press enter to start.");
+    disableRawMode();
 
     getchar();
 }
